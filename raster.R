0aed3d8d7a39d6d2c7168e245cb1107a76abe339
Oscar Perpiñán <oscar@OPL-EUITI>
2013-02-08 15:20:23 +0100

    Mejoro ejemplos en raster.R

--- raster.R
+++ raster.R
@@ -4,15 +4,16 @@ library("sp")
 library("latticeExtra")
 
 proj <- CRS("+proj=longlat +ellps=WGS84")
-##setwd('~/Docencia/eoi/R')
+setwd('~/R/intro')
 
 ##################################################################
 ## CM-SAF
 ##################################################################
 
-unzip("data/SISmm2008_CMSAF.zip", exdir="data/CMSAF")
-old <- setwd("data/CMSAF")
-listFich <- dir(pattern="2008")
+
+old <- setwd("data/")
+unzip("SISmm2008_CMSAF.zip")
+listFich <- dir(pattern=".nc")
 stackSIS <- stack(listFich)
 stackSIS <- stackSIS*24 ##irradiancia (W/m2) a irradiacion Wh/m2
 setwd(old)
@@ -24,7 +25,20 @@ projection(SISmm) <- proj
 names(SISmm) <- month.abb
 
 levelplot(SISmm)
+densityplot(SISmm)
 
+hovmoller(SISmm, dirXY=y,
+          panel=panel.2dsmoother, n=1000)
+hovmoller(SISmm, dirXY=x,
+          panel=panel.2dsmoother, n=1000)
+## No del todo correcto (cada mes tiene un número diferente de días)
+SISy <- mean(SISmm) * 365/1000
+## Mejorado
+SISy <-  sum(SISmm * c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))/1000
+names(SISy) <- 'G0'
+xyplot(G0 ~ y, data=SISy)
+xyplot(G0 ~ x, data=SISy)
+histogram(SISy)
 ##################################################################
 ## SIAR
 ##################################################################
@@ -33,9 +47,10 @@ levelplot(SISmm)
 SIAR <- read.csv("data/SIAR.csv")
 spSIAR <- SpatialPointsDataFrame(SIAR[, c(6, 7)], SIAR[, -c(6, 7)],
                                  proj4str=proj)
+head(spSIAR)
 
-
-levelplot(SISmm, layers='Jun') + layer(sp.points(spSIAR, pch=19, col='black', cex=0.6))
+levelplot(SISy, layers='Jun') +
+  layer(sp.points(spSIAR, pch=19, col='black', cex=0.6))
 
 CMSAF.SIAR <- extract(SISmm, spSIAR)
 CMSAF.SIAR <- zoo(t(CMSAF.SIAR), as.yearmon(idx))
@@ -49,3 +64,21 @@ CMSAFMadrid <- extract(SISmm, spMadrid)
 CMSAFMadrid <- zoo(t(CMSAFMadrid), as.yearmon(idx))
 names(CMSAFMadrid) <- madridSIAR$Estacion
 xyplot(CMSAFMadrid, superpose=TRUE, auto.key=list(space='right'))
+
+
+##################################################################
+## Localización a medida
+##################################################################
+myPoint <- cbind(-3.6, 40.1)
+extract(SISmm, myPoint)
+
+myLocs <- cbind(-8, 38:43)
+SISlocs <- extract(SISmm, myLocs)
+
+levelplot(SISy) + layer(sp.points(myLocs, pch=16, col='black')) 
+
+extent(SISmm)
+myGrid <- expand.grid(long=-10:4, lat=36:44)
+SISgrid <- extract(SISmm, myGrid)
+
+levelplot(SISy) + layer(sp.points(myGrid, pch=16, col='black')) 
