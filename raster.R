306ab397c779c51b1d291c131bc0b181eb5402af
Oscar Perpiñán <oscar@OPL-EUITI>
2013-02-11 13:23:29 +0100

    Datos raster

--- raster.R
+++ raster.R
@@ -1,84 +1,154 @@
-library("raster")
-library("rasterVis")
-library("sp")
+
+## Primeros pasos en R
+## - Configuramos el directorio de trabajo
+
+## Entre las comillas hay que indicar el directorio en el que está el
+## fichero SISmm2008_CMSAF.zip.
+setwd('C:/intro/data')
+
+## - Cargo los paquetes que usaremos
+
+## Si no están instalados hay que usar install.packages('Nombre_del_Paquete')
+library("lattice")
 library("latticeExtra")
 
-proj <- CRS("+proj=longlat +ellps=WGS84")
-setwd('~/R/intro')
+library("sp")
 
-##################################################################
-## CM-SAF
-##################################################################
+library("raster")
+library("rasterVis")
 
+## Leo los ficheros CM~SAF
 
-old <- setwd("data/")
 unzip("SISmm2008_CMSAF.zip")
 listFich <- dir(pattern=".nc")
 stackSIS <- stack(listFich)
-stackSIS <- stackSIS*24 ##irradiancia (W/m2) a irradiacion Wh/m2
-setwd(old)
+## irradiancia (W/m2) a irradiacion Wh/m2
+stackSIS <- stackSIS*24
 
-idx <- seq(as.Date("2008-01-15"), as.Date("2008-12-15"), "month")
+## Añado información temporal
+
+idx <- seq(as.Date("2008-01-15"),
+           as.Date("2008-12-15"),
+           "month")
 
 SISmm <- setZ(stackSIS, idx)
+
+## Fijo la proyección de trabajo y nombres de capas
+
+proj <- CRS("+proj=longlat +ellps=WGS84")
 projection(SISmm) <- proj
 names(SISmm) <- month.abb
 
+## Veamos la información  
+## - Mapa clásico
+
 levelplot(SISmm)
+
+## - Densidad de probabilidad por capa (mes)
+
 densityplot(SISmm)
+
+## Más sobre visualización
+## - Gráfico Hovmoller (tiempo-latitud)
 
 hovmoller(SISmm, dirXY=y,
           panel=panel.2dsmoother, n=1000)
+
+## - Gráfico Hovmoller (tiempo-longitud)
+
 hovmoller(SISmm, dirXY=x,
           panel=panel.2dsmoother, n=1000)
-## No del todo correcto (cada mes tiene un número diferente de días)
+
+## Calculamos el valor anual por celda
+## - No del todo correcto (cada mes tiene un número diferente de días)
+
 SISy <- mean(SISmm) * 365/1000
-## Mejorado
-SISy <-  sum(SISmm * c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))/1000
+
+## - Mejorado
+
+SISy <-  sum(SISmm *
+             c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))/1000
 names(SISy) <- 'G0'
+
+## Veamos la radiación anual
+## - Relación con la longitud y la latitud
+
 xyplot(G0 ~ y, data=SISy)
 xyplot(G0 ~ x, data=SISy)
+
+## - Distribución de valores
+
 histogram(SISy)
-##################################################################
-## SIAR
-##################################################################
 
-## http://solar.r-forge.r-project.org/data/SIAR.csv
-SIAR <- read.csv("data/SIAR.csv")
-spSIAR <- SpatialPointsDataFrame(SIAR[, c(6, 7)], SIAR[, -c(6, 7)],
-                                 proj4str=proj)
-head(spSIAR)
+## Extraemos información de un punto
 
-levelplot(SISy, layers='Jun') +
-  layer(sp.points(spSIAR, pch=19, col='black', cex=0.6))
-
-CMSAF.SIAR <- extract(SISmm, spSIAR)
-CMSAF.SIAR <- zoo(t(CMSAF.SIAR), as.yearmon(idx))
-names(CMSAF.SIAR) <- spSIAR$Estacion
-
-summary(CMSAF.SIAR)
-
-madridSIAR <- subset(SIAR, Provincia == "Madrid")
-spMadrid <- SpatialPoints(madridSIAR[, c('lon', 'lat')], proj4str=proj)
-CMSAFMadrid <- extract(SISmm, spMadrid)
-CMSAFMadrid <- zoo(t(CMSAFMadrid), as.yearmon(idx))
-names(CMSAFMadrid) <- madridSIAR$Estacion
-xyplot(CMSAFMadrid, superpose=TRUE, auto.key=list(space='right'))
-
-
-##################################################################
-## Localización a medida
-##################################################################
 myPoint <- cbind(-3.6, 40.1)
 extract(SISmm, myPoint)
+
+## Extraemos información de varios puntos
 
 myLocs <- cbind(-8, 38:43)
 SISlocs <- extract(SISmm, myLocs)
 
-levelplot(SISy) + layer(sp.points(myLocs, pch=16, col='black')) 
+## - Superponemos mapa global con la localización de los puntos
+
+levelplot(SISy) +
+  layer(sp.points(myLocs,
+                  pch=16, col='black'))
+
+## Extraemos información de una rejilla
 
 extent(SISmm)
 myGrid <- expand.grid(long=-10:4, lat=36:44)
 SISgrid <- extract(SISmm, myGrid)
 
-levelplot(SISy) + layer(sp.points(myGrid, pch=16, col='black')) 
+## - Nuevamente superponemos mapa y rejilla
+
+levelplot(SISy) +
+  layer(sp.points(myGrid,
+                  pch=16, col='black'))
+
+## Estaciones MAGRAMA-SIAR
+## - Descargamos localización de las estaciones SIAR
+
+## Si no hemos descargado el respositorio GitHub
+SIAR <- read.csv("http://solar.r-forge.r-project.org/data/SIAR.csv")
+## Si ya lo hemos descargado está en data
+SIAR <- read.csv("data/SIAR.csv")
+
+## - Construimos un objeto espacial con la información y las coordenadas
+
+spSIAR <- SpatialPointsDataFrame(SIAR[, c(6, 7)],
+                                 SIAR[, -c(6, 7)],
+                                 proj4str=proj)
+head(spSIAR)
+
+## - Mostramos el mapa de radiación anual con las estaciones SIAR
+
+levelplot(SISy, layers='Jun') +
+  layer(sp.points(spSIAR,
+                  pch=19, col='black', cex=0.6))
+
+## Extraemos información de CM~SAF
+
+CMSAF.SIAR <- extract(SISmm, spSIAR)
+CMSAF.SIAR <- zoo(t(CMSAF.SIAR), as.yearmon(idx))
+names(CMSAF.SIAR) <- spSIAR$Estacion
+summary(CMSAF.SIAR)
+
+## Particularizamos para una estación
+## - Primero extraemos información para la estación de Madrid
+
+madridSIAR <- subset(SIAR, Provincia == "Madrid")
+spMadrid <- SpatialPoints(
+              madridSIAR[, c('lon', 'lat')],
+              proj4str=proj)
+CMSAFMadrid <- extract(SISmm, spMadrid)
+CMSAFMadrid <- zoo(t(CMSAFMadrid), as.yearmon(idx))
+names(CMSAFMadrid) <- madridSIAR$Estacion
+
+## - Mostramos la serie temporal correspondiente
+
+xyplot(CMSAFMadrid,
+       superpose=TRUE,
+       auto.key=list(space='right'))
