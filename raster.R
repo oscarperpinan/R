0724cf0bda437ba82c6de3a15984ef4309d7d8fb
Oscar Perpiñán <oscar.perpinan@gmail.com>
2014-01-09 11:02:26 +0100

    Actualizo raster y zoo

--- raster.R
+++ raster.R
@@ -1,24 +1,18 @@
-
-## Primeros pasos en R
-## - Configuramos el directorio de trabajo
-
 ## Entre las comillas hay que indicar el directorio en el que está el
-## fichero SISmm2008_CMSAF.zip.
-setwd('C:/intro/data')
-
-## - Cargo los paquetes que usaremos
+## repositorio (será visible la carpeta data/)
+setwd('~/R/intro/')
 
 ## Si no están instalados hay que usar install.packages('Nombre_del_Paquete')
-library("lattice")
-library("latticeExtra")
-
+## Clases y métodos para datos espaciales
 library("sp")
-
 library("ncdf")
 library("raster")
+## Series temporales
+library("zoo")
+## Gráficos
+library("lattice")
+library("latticeExtra")
 library("rasterVis")
-
-## Leo los ficheros CM~SAF
 
 unzip("SISmm2008_CMSAF.zip")
 listFich <- dir(pattern=".nc")
@@ -26,114 +20,70 @@ stackSIS <- stack(listFich)
 ## irradiancia (W/m2) a irradiacion Wh/m2
 stackSIS <- stackSIS*24
 
-## Añado información temporal
-
 idx <- seq(as.Date("2008-01-15"),
            as.Date("2008-12-15"),
            "month")
 
 SISmm <- setZ(stackSIS, idx)
 
-## Fijo la proyección de trabajo y nombres de capas
-
 proj <- CRS("+proj=longlat +ellps=WGS84")
 projection(SISmm) <- proj
 names(SISmm) <- month.abb
 
-## Veamos la información  
-## - Mapa clásico
-
 levelplot(SISmm)
 
-## - Densidad de probabilidad por capa (mes)
-
 densityplot(SISmm)
-
-## Más sobre visualización
-## - Gráfico Hovmoller (tiempo-latitud)
 
 hovmoller(SISmm, dirXY=y,
           panel=panel.2dsmoother, n=1000)
 
-## - Gráfico Hovmoller (tiempo-longitud)
-
 hovmoller(SISmm, dirXY=x,
           panel=panel.2dsmoother, n=1000)
 
-## Calculamos el valor anual por celda
-## - No del todo correcto (cada mes tiene un número diferente de días)
-
 SISy <- mean(SISmm) * 365/1000
-
-## - Mejorado
 
 SISy <-  sum(SISmm *
              c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31))/1000
 names(SISy) <- 'G0'
 
-## Veamos la radiación anual
-## - Relación con la longitud y la latitud
-
 xyplot(G0 ~ y, data=SISy)
 xyplot(G0 ~ x, data=SISy)
 
-## - Distribución de valores
-
 histogram(SISy)
-
-## Extraemos información de un punto
 
 myPoint <- cbind(-3.6, 40.1)
 extract(SISmm, myPoint)
 
-## Extraemos información de varios puntos
-
 myLocs <- cbind(-8, 38:43)
 SISlocs <- extract(SISmm, myLocs)
-
-## - Superponemos mapa global con la localización de los puntos
 
 levelplot(SISy) +
   layer(sp.points(myLocs,
                   pch=16, col='black'))
 
-## Extraemos información de una rejilla
-
 extent(SISmm)
 myGrid <- expand.grid(long=-10:4, lat=36:44)
 SISgrid <- extract(SISmm, myGrid)
-
-## - Nuevamente superponemos mapa y rejilla
 
 levelplot(SISy) +
   layer(sp.points(myGrid,
                   pch=16, col='black'))
 
-## Estaciones MAGRAMA-SIAR
-SIAR <- read.csv("SIAR.csv")
-
-## - Construimos un objeto espacial con la información y las coordenadas
+SIAR <- read.csv("data/SIAR.csv")
 
 spSIAR <- SpatialPointsDataFrame(SIAR[, c(6, 7)],
                                  SIAR[, -c(6, 7)],
                                  proj4str=proj)
 head(spSIAR)
 
-## - Mostramos el mapa de radiación anual con las estaciones SIAR
-
 levelplot(SISy, layers='Jun') +
   layer(sp.points(spSIAR,
                   pch=19, col='black', cex=0.6))
-
-## Extraemos información de CM~SAF
 
 CMSAF.SIAR <- extract(SISmm, spSIAR)
 CMSAF.SIAR <- zoo(t(CMSAF.SIAR), as.yearmon(idx))
 names(CMSAF.SIAR) <- spSIAR$Estacion
 summary(CMSAF.SIAR)
-
-## Particularizamos para una estación
-## - Primero extraemos información para la estación de Madrid
 
 madridSIAR <- subset(SIAR, Provincia == "Madrid")
 spMadrid <- SpatialPoints(
@@ -142,8 +92,6 @@ spMadrid <- SpatialPoints(
 CMSAFMadrid <- extract(SISmm, spMadrid)
 CMSAFMadrid <- zoo(t(CMSAFMadrid), as.yearmon(idx))
 names(CMSAFMadrid) <- madridSIAR$Estacion
-
-## - Mostramos la serie temporal correspondiente
 
 xyplot(CMSAFMadrid,
        superpose=TRUE,
--- zoo.R
+++ zoo.R
@@ -1,7 +1,3 @@
-
-## Lectura de datos con =read.table=
-## - Primero lo intentamos con la versión final
-
 dats <- read.table('data/aranjuez.csv')
 head(dats)
 
@@ -17,8 +13,6 @@ head(aranjuez)
 class(aranjuez)
 names(aranjuez)
 
-## Visualización de datos
-
 library(lattice)
 
 xyplot(Radiation ~ TempAvg, data=aranjuez)
@@ -30,8 +24,6 @@ xyplot(Radiation ~ TempAvg + TempMax + TempMin,
        data = aranjuez, xlab='Temperature',
        type=c('p', 'r'), auto.key=TRUE,
        pch=16, alpha=0.5)
-
-## Visualización de datos (advanced!)
 
 library(RColorBrewer)
 
@@ -47,8 +39,6 @@ xyplot(Radiation ~ TempAvg + TempMax + TempMin,
        par.settings=custom.theme(pch=16,
          alpha=0.8, col=myPal))
 
-## Transformamos a serie temporal
-
 library(zoo)
 
 fecha <- as.POSIXct(aranjuez[,1],
@@ -59,8 +49,6 @@ aranjuez <- zoo(aranjuez[, -1], fecha)
 class(aranjuez)
 head(aranjuez)
 
-## Leemos directamente como serie temporal
-
 aranjuez <- read.zoo('data/aranjuez.csv',
                      sep=',', header=TRUE)
 
@@ -68,33 +56,21 @@ header(aranjuez)
 names(aranjuez)
 summary(index(aranjuez))
 
-## Ahora con la versión original
-## - Primero descomprimimos el archivo
-
 unzip('data/InformeDatos.zip', exdir='data')
-
-## - Y ahora abrimos teniendo en cuenta codificación, separadores, etc.
 
 aranjuez <- read.table("data/M03_Aranjuez_01_01_2004_31_12_2011.csv",
                      fileEncoding = 'UTF-16LE',
                      header = TRUE, fill = TRUE,
                      sep = ';', dec = ",")
 
-## - Vemos el contenido
-
 head(aranjuez)
 summary(aranjuez)
 names(aranjuez)
-
-## Convertimos a serie temporal
-## - Sólo nos interesan algunas variables (indexamos por columnas)
 
 tt <- as.Date(aranjuez$Fecha, format='%d/%m/%Y')
 aranjuez <- zoo(aranjuez[, c(6, 7, 9, 11, 12, 16,
                              17, 19, 20, 22)],
                 order.by=tt)
-
-## Ajustamos los nombres (opcional)
 
 names(aranjuez) <- c('TempAvg', 'TempMax',
                      'TempMin', 'HumidAvg',
@@ -102,28 +78,16 @@ names(aranjuez) <- c('TempAvg', 'TempMax',
                      'WindMax', 'Radiation',
                      'Rain', 'ET')
 
-## Nuevamente mostramos datos
-## - Método simple
-
 xyplot(aranjuez)
-
-## - Seleccionamos variables y superponemos
 
 xyplot(aranjuez[,c("TempAvg", "TempMax", "TempMin")],
        superpose=TRUE)
 
-## - Para cruzar variables hay que convertir a =data.frame=
-
 xyplot(TempAvg ~ Radiation,
        data=as.data.frame(aranjuez))
 
-## Limpieza de datos
-## - Conversión de Unidades (MJ -> Wh)
-
 aranjuez$G0 <- aranjuez$Radiation/3.6*1000
 xyplot(aranjuez$G0)
-
-## - Filtrado de datos
 
 aranjuezClean <- within(as.data.frame(aranjuez),{
   TempMin[TempMin>40] <- NA
@@ -134,14 +98,9 @@ aranjuezClean <- within(as.data.frame(aranjuez),{
 
 aranjuez <- zoo(aranjuezClean, index(aranjuez))
 
-## Media anual
-## - Primero definimos una función para extraer el año
-
 Year <- function(x)as.numeric(format(x, "%Y"))
 
 Year(index(aranjuez))
-
-## - Y la empleamos para agrupar con =aggregate=
 
 aranjuezY <- aggregate(aranjuez$G0, by=Year,
                        FUN=mean, na.rm=TRUE)
@@ -152,8 +111,8 @@ G0y <- aggregate(aranjuez$G0, by=Year,
                  FUN=mean, na.rm=TRUE)
 G0y
 
-## Medias mensuales
-## - Meses como números
+aggregate(aranjuez$G0, by=function(tt)cut(tt, 'year'),
+          FUN=mean, na.rm=TRUE)
 
 Month <- function(x)as.numeric(format(x, "%m"))
 
@@ -163,16 +122,11 @@ G0m <- aggregate(aranjuez$G0, by=Month,
                  FUN=mean, na.rm=TRUE)
 G0m
 
-## - Meses como etiquetas
-
 months(index(aranjuez))
 
 G0m <- aggregate(aranjuez$G0, by=months,
                  FUN=mean, na.rm=TRUE)
 G0m
-
-## Medias mensuales para cada año
-## - La función para agrupar es =as.yearmon=
 
 as.yearmon(index(aranjuez))
 
@@ -180,13 +134,8 @@ G0ym <- aggregate(aranjuez$G0, by=as.yearmon,
                   FUN=mean, na.rm=TRUE)
 G0ym
 
-## Ejemplo: Lanai-Hawaii
-
 URL <- "http://www.nrel.gov/midc/apps/plot.pl?site=LANAI&start=20090722&edy=19&emo=11&eyr=2010&zenloc=19&year=2010&month=11&day=1&endyear=2010&endmonth=11&endday=19&time=1&inst=3&inst=4&inst=5&inst=10&type=data&first=3&math=0&second=-1&value=0.0&global=-1&direct=-1&diffuse=-1&user=0&axis=1"
 ## URL <- "data/NREL-Hawaii.csv"
-
-## Leemos como serie temporal
-## - Leemos con =read.zoo=
 
 lat <- 20.77
 lon <- -156.9339
@@ -202,17 +151,11 @@ hawaii <- read.zoo(URL,
                   tz = "HST"), 
                 header=TRUE, sep=",")
 
-## - Añadimos Directa en el plano Horizontal
-
 hawaii$B0 <- with(hawaii, G0-D0)
-
-## Mostramos datos como serie temporal
 
 xyplot(hawaii)
 xyplot(hawaii[,c('G0', 'D0', 'B0')],
        superpose=TRUE)
-
-## Mostramos relaciones entre variables
 
 xyplot(Ta ~ G0 + D0 + B0,
        data=as.data.frame(hawaii),
@@ -223,18 +166,11 @@ xyplot(Ta ~ G0 + D0 + B0,
        outer=TRUE, layout=c(3, 1),
        scales=list(x=list(relation='free')))
 
-## Irradiación horaria
-## - Primer intento
-
 hour <- function(x)as.numeric(format(x, '%H'))
 
 G0h <- aggregate(hawaii$G0, by=hour,
                  FUN=sum, na.rm=1)/1000
 G0h
-
-## Irradiación horaria
-
-## - Mejor así
 
 hour <- function(x)as.POSIXct(format(x,
                                      '%Y-%m-%d %H:00:00'))
@@ -243,14 +179,9 @@ G0h <- aggregate(hawaii$G0, by=hour,
                  FUN=sum, na.rm=1)/60
 G0h
 
-## Irradiación diaria
-## - A partir de la horaria
-
 G0d <- aggregate(G0h,
                  by=function(x)format(x, '%Y-%m-%d'),
                  sum)/1000
-
-## - A partir de la minutaria
 
 day <- function(x)format(x, '%Y-%m-%d')
 G0d <- aggregate(hawaii$G0, by=day,
@@ -261,8 +192,6 @@ truncDay <- function(x)as.POSIXct(trunc(x, units='day'))
 G0d <- aggregate(hawaii$G0, by=truncDay,
                  sum)/60/1000
 G0d
-
-## Más complicado: agrupar por 30 minutos
 
 halfHour <- function(tt, delta=30){
   tt <- as.POSIXlt(tt)
